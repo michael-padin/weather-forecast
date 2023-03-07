b84cf0334cb69c7b4523c17eb4f273429aa4bdb3
Michael <padinmichael201@gmail.com>
2023-03-07 21:39:36 +0800

    feat: add home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,24 @@
- import React from 'react'
- 
- const Home = () => {
-   return (
-     <div>Home</div>
-   )
- }
- 
- export default Home
+import React from "react";
+
+import { DisplayProfile, SearchCity } from "../components";
+
+const Home = () => {
+  const handleChange = () => {};
+
+  const handleSearch = () => {
+
+  }
+
+  return (
+    <div className="flex flex-col lg:w-auto justify-center flex-srap">
+      <div className="sm:hidden md:lg:block w-[100%] flex text-[18px] justify-center">
+        <DisplayProfile name="Michael Padin " nickname="michael-padin" />
+      </div>
+      <div className="mt-[100px] flex items-center justify-center ">
+        <SearchCity handleChange={handleChange} handleSearch={handleSearch} />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
