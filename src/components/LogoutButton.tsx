780c82bb9099ecbabe14d9e3255e2862b608fec3
Michael <padinmichael201@gmail.com>
2023-03-08 04:55:54 +0800

    feat: add logout button functionality

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
-  return (
-    <div>LogoutButton</div>
-  )
-}
+  const { logout } = useAuth0();
 
-export default LogoutButton
+  return (
+    <button
+      className="border-[2px] border-[#118be4] sm:px-[1rem] md:lg:px-[2rem] sm:py-[.5rem] md:py-[.8rem] text-[18px] bg-blue-50 text-[#118be4] rounded-md font-bold"
+      onClick={() => logout()}
+    >
+      Logout
+    </button>
+  );
+};
+
+export default LogoutButton;
