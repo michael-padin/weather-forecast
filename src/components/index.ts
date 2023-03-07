e69abc24c00a0c0907b2c4a6589d4e77a79099d9
Michael <padinmichael201@gmail.com>
2023-03-07 21:44:42 +0800

    refactor: add all components to ts file for ease of access

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export { default as Navbar } from "./Navbar";
 export { default as ProtectedRoute } from "./ProtectedRoute";
 export { default as LogoutButton } from "./LogoutButton";
 export { default as LoginButton } from "./LoginButton";
+export { default as DisplayProfile } from "./DisplayProfile";
+export { default as SearchCity } from "./SearchCity";
+export { default as WeatherDetails } from "./WeatherDetails";
