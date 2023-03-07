9064b275cef231db5f4e2c196455eb34750ceeb3
Michael <padinmichael201@gmail.com>
2023-03-08 04:55:21 +0800

    feat: add github info component

--- src/components/DisplayProfile.tsx
+++ src/components/DisplayProfile.tsx
@@ -1,17 +1,23 @@
-import React from "react";
 
 interface IDisplayProfile {
-  name: string;
-  nickname: string;
+  name?: string;
+  nickname?: string;
 }
 
 const DisplayProfile = ({ name, nickname }: IDisplayProfile) => {
   return (
     <div className="flex flex-col mt-[50px] gap-5 items-center">
       <p>{name}</p>
-      <a href={`https://github.com/${nickname}`}>
-        {`https://github.com/${nickname}`}
-      </a>
+
+      {nickname && (
+        <a
+          href={`https://github.com/${nickname}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {`https://github.com/${nickname}`}
+        </a>
+      )}
     </div>
   );
 };
