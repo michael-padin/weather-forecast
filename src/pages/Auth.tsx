fa24e6ae11876f62f9cfed94bef196720529f443
Michael <padinmichael201@gmail.com>
2023-03-08 04:59:03 +0800

    feat: add user page redirection

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Navigate } from "react-router-dom";
 import { LoginButton } from "../components";
 
 const Auth = () => {
+  const { isAuthenticated } = useAuth0();
+
+  if (isAuthenticated) {
+    return <Navigate to="/" replace />;
+  }
+
   return (
     <div className="w-[100%] flex flex-col">
       <div className="text-left flex max-w-[700px] mt-[100px] sm:px-8 lg:mt-[150px] mx-auto flex-col gap-[2rem] text">
