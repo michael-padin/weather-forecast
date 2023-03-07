5cd6a5763c889aee1539f75f47510accc576e082
Michael <padinmichael201@gmail.com>
2023-03-08 04:55:43 +0800

    feat: add login button functionality

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
 
 const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
-    <button className="px-[2rem] py-[1.2rem]">LoginButton</button>
-  )
-}
+    <button
+      className="px-[2rem] py-[.5rem] lg:px-[4rem] lg:py-[.8rem] text-[18px] bg-[#118be4] text-white rounded-md font-bold"
+      onClick={() => loginWithRedirect()}
+    >
+      Login
+    </button>
+  );
+};
 
-export default LoginButton
+export default LoginButton;
