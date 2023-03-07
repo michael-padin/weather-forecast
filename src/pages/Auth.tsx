84233010353c7a3f3a77ab18c456052a29ee47ba
Michael <padinmichael201@gmail.com>
2023-03-07 21:43:51 +0800

    style: update style on auth page for responsiveness

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -4,13 +4,13 @@ import { LoginButton } from "../components";
 const Auth = () => {
   return (
     <div className="w-[100%] flex flex-col">
-      <div className="text-left flex max-w-[700px] mt-[150px] mx-auto flex-col gap-[2rem] text">
-        <p className="text-xl">
+      <div className="text-left flex max-w-[700px] mt-[100px] sm:px-8 lg:mt-[150px] mx-auto flex-col gap-[2rem] text">
+        <p className="text-[18px]">
           Welcome to the weather forecast web application. Please Login with
           your Github user to use the application and view the weather in you
           city.
         </p>
-        <div className="flex">
+        <div className="flex mt-[20px]">
           <LoginButton />
         </div>
       </div>
