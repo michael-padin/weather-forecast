5f1668bac3c9644b26d2ee19add78b1b3731a892
Michael <padinmichael201@gmail.com>
2023-03-08 05:00:36 +0800

    feat: add functionality for displaying data

--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -1,24 +1,74 @@
-import React from "react";
-import { useNavigate } from "react-router";
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
 import { WeatherDetails } from "../components";
 
+interface IWeatherDetails {
+  date: string;
+  temp: number;
+  desc: string;
+  main: string;
+  pressure: number;
+  humid: number;
+}
+
 const Weather = () => {
+  const [weatherDetails, setWeatherDetails] = useState<IWeatherDetails>({
+    date: "",
+    temp: 0,
+    desc: "",
+    main: "",
+    pressure: 0,
+    humid: 0,
+  });
+  const location = useLocation();
   const navigate = useNavigate();
+  const { state } = location;
+
+  const {date, temp, desc, main, pressure, humid} = weatherDetails
+
+  useEffect(() => {
+
+    // redirect if there are no state available in the location
+    if (!state) navigate("/")
+    
+    setWeatherDetails((prevVal) => {
+      return {
+        ...prevVal,
+        date: new Date(state.dt * 1000).toLocaleDateString(),
+        temp: state.main.temp,
+        desc: state.weather[0].description,
+        main: state.weather[0].main,
+        pressure: state.main.pressure,
+        humid: state.main.humidity,
+      };
+    });
+
+    return () => {
+      setWeatherDetails({
+        date: "",
+        temp: 0,
+        desc: "",
+        main: "",
+        pressure: 0,
+        humid: 0,
+      });
+    };
+  }, [navigate, state]);
 
   return (
-    <div className="flex items-center justify-center">
-      <div>
+    <div className="flex items-center justify-center pb-5">
+      <div className="sm:w-full sm:px-5 md:lg:w-auto">
         <WeatherDetails
-          date="02/21/2024"
-          temp="85"
-          desc="Nice Cloud"
-          main="clear"
-          pressure="100"
-          humidity="100"
+          date={date}
+          temp={temp}
+          desc={desc}
+          main={main}
+          pressure={pressure}
+          humidity={humid}
         />
-        <div className="flex mt-[200px] justify-end">
+        <div className="flex sm:mt-[100px] md:mt-[200px] justify-end">
           <button
-            className="px-[3rem] py-[.8rem] text-[18px] bg-[#118be4] text-white rounded-md font-bold"
+            className="sm:px-[2rem] sm:text-[16px] py-[.5rem] md:px-[3rem] md:py-[.8rem] text-[18px] bg-[#118be4] text-white rounded-md font-bold"
             onClick={() => navigate("/")}
           >
             Back
