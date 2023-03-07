e997c0ab7e70a9d5101c2b7b5b486ce444d1dfdd
Michael <padinmichael201@gmail.com>
2023-03-07 21:41:32 +0800

    feat: add weather details page

--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router";
+import { WeatherDetails } from "../components";
 
 const Weather = () => {
-  return (
-    <div>Weather</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Weather
+  return (
+    <div className="flex items-center justify-center">
+      <div>
+        <WeatherDetails
+          date="02/21/2024"
+          temp="85"
+          desc="Nice Cloud"
+          main="clear"
+          pressure="100"
+          humidity="100"
+        />
+        <div className="flex mt-[200px] justify-end">
+          <button
+            className="px-[3rem] py-[.8rem] text-[18px] bg-[#118be4] text-white rounded-md font-bold"
+            onClick={() => navigate("/")}
+          >
+            Back
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
