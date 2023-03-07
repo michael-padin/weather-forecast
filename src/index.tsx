242ff44df7494c28098e0aee0ba590f413e17534
Michael <padinmichael201@gmail.com>
2023-03-07 21:46:57 +0800

    feat: add auth0 for authentication

--- src/index.tsx
+++ src/index.tsx
@@ -12,12 +12,12 @@ root.render(
   <React.StrictMode>
     <Router>
       <Auth0Provider
-        domain={"dev-v4aa8h6wri4nh0r1.us.auth0.com"}
-        clientId={"I1X4WrpxtiTkt8h7SNZJI5C3ROH4Szgn"}
+        domain="dev-on86dq15406z8lnz.us.auth0.com"
+        clientId="v0uAxdGYuIfrOGr1HY9CZMuHhWaZg59z"
         authorizationParams={{
           redirect_uri: window.location.origin,
         }}
-      >
+      > 
         <App />
       </Auth0Provider>
     </Router>
