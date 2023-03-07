d664c73b1d5fe524dcf615c006bda1e81048ee3f
Michael <padinmichael201@gmail.com>
2023-03-07 21:37:51 +0800

    feat: add navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,15 +5,15 @@ import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between px-[1rem] py-[1rem]">
+    <div className="flex justify-between px-[1rem] py-[1rem] bg-slate-200">
       <div className="flex gap-2 items-center">
-        <div>
-          <img src={logo} alt="Weather Logo" />
+        <div className="lg:w-[70px] sm:w-[50px]">
+          <img src={logo} alt="Weather Logo" className="h-full w-full" />
         </div>
-        <h1 className="text-3xl font-bold">Weather Forecast</h1>
+        <h1 className="text-3xl sm:text-2xl font-bold">Weather Forecast</h1>
       </div>
-      <div>
-        <LogoutButton />
+      <div className="flex items-center">
+        {/* <LogoutButton /> */}
       </div>
     </div>
   );
